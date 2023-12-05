98c995193f67e5628b2b09f63eeef5f822b03382
Artheweeb <118442170+Artheweeb@users.noreply.github.com>
2023-12-05 22:32:48 +0500

    Update vkio.py

--- vkio.py
+++ vkio.py
@@ -2,17 +2,16 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from random import randint
 
-TOKEN = 'vk1.a.qOQPyAdJ_Z5WwjzbNl_WFUq2P05QGpwj-537I7vwLTneH1Fz06BBEslq0_rbUGJFabRakR9V-pL7dzhhx6qCeHA-AP2wNndJTFHYQ7sKmPyiAB05KWIkfmH4G_Gl9luw3qqe8UvwB6tTTaojNW1EcIHjgP8uX5Z89ppE5Mv2cpaWrEmrWtD9b9GC1ulJ_viLiTwOfjTcBd4mqifQqazVZw'
-GROUP_ID = 219645807
-
 
 class VKInputOutputClass:
     def __init__(self):
-        self.session = vk_api.VkApi(token=TOKEN)
+        with open('vk_community_data.txt', 'r') as data:
+            self.community_data = (data.readline().strip(), int(data.readline()))
+        self.session = vk_api.VkApi(token=self.community_data[0])
         self.long_poll = None
 
     def start(self):
-        self.long_poll = VkBotLongPoll(self.session, GROUP_ID)
+        self.long_poll = VkBotLongPoll(self.session, self.community_data[1])
 
     def send_message(self, user_id, message):
         self.session.get_api().messages.send(user_id=user_id, message=message, random_id=randint(0, 2 ** 64))
